f443c56eb413e1d86480e47b1323158cc765bd97
Abzal Orazbek <me@abzy.kz>
2026-05-04 23:10:34 +0500

    fix treesitter

--- dot_config/nvim/lua/abzy/plugins/treesitter.lua
+++ dot_config/nvim/lua/abzy/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		opts = {
-			ensure_installed = {
+		config = function()
+			local parsers = {
 				"bash",
 				"c",
 				"diff",
@@ -17,13 +18,23 @@ return {
 				"vim",
 				"vimdoc",
 				"hyprlang",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
+			}
+
+			require("nvim-treesitter").setup {
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			}
+
+			if vim.fn.executable("tree-sitter") == 1 then
+				require("nvim-treesitter").install(parsers)
+			end
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = parsers,
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	}
 }
