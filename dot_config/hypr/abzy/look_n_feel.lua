1bcd07a82ead870dd7740bf723abbba9d5c171a1
Abzal Orazbek <me@abzy.kz>
2026-07-31 15:28:08 +0500

    fix animations

--- dot_config/hypr/abzy/look_n_feel.lua
+++ dot_config/hypr/abzy/look_n_feel.lua
@@ -44,6 +44,6 @@ hl.config({
 
 -- Animations
 hl.curve("easeInAndOut", { type = "bezier", points = { { 0.83, 0 }, { 0.17, 1 } } })
-hl.curve("defaultSpring", { type = "spring", mass = 1, stiffness = 800, dampening = 60 })
+hl.curve("defaultSpring", { type = "spring", mass = 1, stiffness = 1200, dampening = 70 })
 hl.animation({ leaf = "global", enabled = true, speed = 1, spring = "defaultSpring" })
 --hl.animation({ leaf = "global", enabled = true, speed = 2, bezier = "easeInAndOut" })
